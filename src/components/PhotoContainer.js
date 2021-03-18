f18411c4ebf1a2072e7d20cd1605444433d91703
Toye Ogundepo <ogundepo@gmail.com>
2021-03-18 00:45:26 -0500

    debug NotFound Component functionality.

--- src/components/PhotoContainer.js
+++ src/components/PhotoContainer.js
@@ -3,37 +3,55 @@ import Photo from "./Photo";
 import NotFound from "./NotFound";
 import apiKey from "../config";
 
+
 class PhotoContainer extends React.Component {
   constructor(props) {
-    super(props);
+    super(props)
   }
-
+  
   render() {
-    const results = this.props.images.photo;
-
+    const results = this.props.images.photo
     let search = this.props.match.params.searchValue;
     let whatToDisplay;
-    if (this.props.images.photo) {
-      whatToDisplay = results.map((image) => {
-        return (
-          <Photo
-            url={`https://live.staticflickr.com/${image.server}/${image.id}_${image.secret}.jpg`}
-            alt={image.title}
-            key={image.id}
-          />
-        );
-      });
+    if (results !== undefined) {
+      whatToDisplay = results.map(image => { return <Photo url={`https://live.staticflickr.com/${image.server}/${image.id}_${image.secret}.jpg`} alt={image.title} key={ image.id } /> })
     } else {
-      whatToDisplay = <NotFound />;
+      whatToDisplay = <NotFound />
     }
+
     return (
       <div className="photo-container">
         <h2>Results: {search}</h2>
         <h3>{this.props.title}</h3>
-        <ul>{whatToDisplay}</ul>
+        <ul>
+
+        { 
+          whatToDisplay
+        }  
+        
+          {/* <li>
+            <img
+              src="https://farm5.staticflickr.com/4334/37032996241_4c16a9b530.jpg"
+              alt=""
+            />
+          </li> */}
+
+          {/* <!-- Not Found --> */}
+          {/* <li class="not-found">
+            <h3>No Results Found</h3>
+            <p>You search did not return any results. Please try again.</p>
+          </li> */}
+        </ul>
       </div>
     );
   }
+  
+
+  
+  
+  
+  // 
+    
 }
 
 export default PhotoContainer;
